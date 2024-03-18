0a0852910b6640641789d4d149f7a3a2d06b743a
Chetan Lokare <chetanlokare15@gmail.com>
2024-03-18 14:56:16 +0000

    -

--- booklib/src/App.jsx
+++ booklib/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
+import Manage from './pages/Manage';
+import Home from './pages/Home';
 
 function App() {
-
+  let Component;
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home;
+      break;
+    case "/manage":
+      Component = Manage;
+      break;  
+    
+    default:
+      break;
+  }
   return (
     <>
+      <Navbar />
+      <div className="container">
+        <Component />
+      </div>
     </>
   )
 }
---
+++ booklib/src/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">booklib.</a>
+        <ul>
+            <CustomLink href="/manage">Manage Books</CustomLink>
+        </ul>
+    </nav>
+}
+
+function CustomLink({ href, children, ...props }) {
+    const path = window.location.pathname;
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props}>
+                {children}
+            </a>
+        </li>
+    )
+}
